8f9876a319f9079cf1aa150a73ff23a0c69e2b15
himanshu619619 <48987618+himanshu619619@users.noreply.github.com>
2020-03-21 12:51:24 +0530

    website

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@
 		<section class="content-area">
 			<div class="container">
 				<div class="row">
-					<div class="site-main col-sm-9 alignleft">
+					<div class="site-main col-sm-12 alignleft">
 						<div class="video-container">
 							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3493.513248652066!2d76.62003501484865!3d28.88309267944768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d84dee1290973%3A0x131b8ad61ff80398!2sAgro%20Mall!5e0!3m2!1sen!2sin!4v1584698300367!5m2!1sen!2sin" width="600" height="450" style="border: 0; pointer-events: none;" allowfullscreen=""></iframe>
 						</div>
@@ -77,17 +77,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="widget-area col-sm-3 align-left">
-						<aside class="widget widget_text">
-							<img src="images/images-sidebar/sidebanner-ticketing.jpg" alt="">
-						</aside>
-						<aside class="widget widget_text">
-							<img src="images/images-sidebar/sidebanner-tour.png" alt="">
-						</aside>
-						<aside class="widget widget_text">
-							<img src="images/images-sidebar/hertz-sidebanner.jpg" alt="">
-						</aside>
-					</div>
+				
 				</div>
 			</div>
 		</section>
